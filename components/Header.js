38a0f4abf5b4e1b7daaf3a1c719e209742b028f9
wangjianbo <708873100@qq.com>
2021-12-25 13:23:25 +0800

    2st commit

--- components/Header.js
+++ components/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import sty from '../public/style/components/header.module.css'
 import { Menu, Row, Col, } from 'antd';
-import { MailOutlined, CoffeeOutlined, HomeOutlined,BulbOutlined } from '@ant-design/icons';
+import { MailOutlined, CoffeeOutlined, HomeOutlined, BulbOutlined } from '@ant-design/icons';
 
 
 const { SubMenu } = Menu;
@@ -23,15 +23,17 @@ class Header extends React.Component {
             <div className={sty.header}>
                 <Row type="flex" justify='center' >
                     <Col xs={24} sm={24} md={10} lg={10} xl={10}>
-                        <span className={sty.headerlogo}>王健博</span>
+                        <a href='/'>
+                            <span className={sty.headerlogo}>王健博</span>
+                        </a>
                         <span className={sty.headertxt}>大龄双非渣硕的个人空间</span>
                     </Col>
                     <Col xs={0} sm={0} md={14} lg={8} xl={8}>
                         <Menu className={sty.menu} mode='horizontal'>
-                            <Menu.Item className={sty.menuitem}  key="index" icon={<HomeOutlined />} id={sty.juzhong}>
+                            <Menu.Item className={sty.menuitem} key="index" icon={<HomeOutlined />} id={sty.juzhong}>
                                 个人主页
                             </Menu.Item>
-                            <Menu.Item className={sty.menuitem}  key="interviewLeetcode" icon={<BulbOutlined />} id={sty.juzhong}>
+                            <Menu.Item className={sty.menuitem} key="interviewLeetcode" icon={<BulbOutlined />} id={sty.juzhong}>
                                 刷题面经
                             </Menu.Item>
                             <Menu.Item className={sty.menuitem} key="dailyLife" icon={<CoffeeOutlined />} >
--- pages/Detailed.js
+++ pages/Detailed.js
@@ -1,7 +1,7 @@
 
 import React, { useState } from 'react'
 import Head from 'next/head'
-import { Button, Row, Col, List, Breadcrumb, Divider,Affix} from 'antd'
+import { Button, Row, Col, List, Breadcrumb, Divider, Affix } from 'antd'
 import Header from '../components/Header'
 import { CalendarTwoTone, FolderOpenTwoTone, FireTwoTone } from '@ant-design/icons'
 import sty from '../public/style/pages/detailed.module.css'
@@ -9,53 +9,32 @@ import Author from '../components/Author'
 import MySkill from '../components/Myskills'
 import Footer from '../components/Footer'
 import ReactMarkdown from 'react-markdown'
-import MarkdownNavbar from 'markdown-navbar';
+import MarkdownNavbar from 'markdown-navbar'
+import axios from 'axios'
+import marked from 'marked'
+import hljs from 'highlight.js'
+import 'highlight.js/styles/monokai-sublime.css';
 
 
-const MyList = () => {
-  const [myList, setMyList] = useState(
-    [
-      { title: '50元加入小密圈 胖哥带你学一年', context: '50元跟着胖哥学一年，掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。' },
-      { title: 'React实战视频教程-技术胖Blog开发(更新04集)', context: '50元跟着胖哥学一年，掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。' },
-      { title: 'React服务端渲染框架Next.js入门(共12集)', context: '50元跟着胖哥学一年，掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。' },
-      { title: 'React Hooks 免费视频教程(共11集)', context: '50元跟着胖哥学一年，掌握程序人的学习方法。 也许你刚步入IT行业，也许你遇到了成长瓶颈，也许你不知道该学习什么知识，也许你不会融入团队，也许...........有些时候你陷入彷徨。 你需要一个强力的队友，你需要一个资深老手，你需要一个随时可以帮助你的人，你更需要一个陪你加速前行的。 我在这个行业走了12年，从后端、前端到移动端都从事过，从中走了很多坑，但我有一套适合程序员的学习方法。 如果你愿意，我将带着你在这个程序行业加速奔跑。分享我学习的方法，所学的内容和一切我的资料。 你遇到的职业问题，我也会第一时间给你解答。我需要先感谢一直帮助我的小伙伴，这个博客能产出300多集免费视频，其中有他们的鼎力支持，如果没有他们的支持和鼓励，我可能早都放弃了。 原来我博客只是录制免费视频，然后求30元的打赏。 每次打赏我都会觉得内疚，因为我并没有给你特殊的照顾，也没能从实质上帮助过你。 直到朋友给我介绍了知识星球，它可以专享加入，可以分享知识，可以解答问题，所以我如获珍宝，决定把打赏环节改为知识服务。我定价50元每年，为什么是50元每年？因为这是知识星球允许的最低收费了。' },
-    ]
-  )
-  let markdown = '#' +
-    '# P01:课程介绍和环境搭建\n' +
-    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
-    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
-    '**这是加粗的文字**\n\n' +
-    '*这是倾斜的文字*`\n\n' +
-    '***这是斜体加粗的文字***\n\n' +
-    '~~这是加删除线的文字~~ \n\n' +
-    '\`console.log(111)\` \n\n' +
-    '# p02:来个Hello World 初始Vue3.0\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n' +
-    '***\n\n\n' +
-    '# p03:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p04:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '#5 p05:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p06:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '# p07:Vue3.0基础知识讲解\n' +
-    '> aaaaaaaaa\n' +
-    '>> bbbbbbbbb\n' +
-    '>>> cccccccccc\n\n' +
-    '``` var a=11; ```'
+const Detailed = (props) => {
+  console.log(props)
+  const renderer = new marked.Renderer();
+
+  marked.setOptions({
+      renderer: renderer,
+      gfm: true,
+      pedantic: false,
+      sanitize: false,
+      tables: true,
+      breaks: false,
+      smartLists: true,
+      smartypants: false,
+      highlight: function (code) {
+        return hljs.highlightAuto(code).value;
+      }
+  });
+
+  let html = marked(props.content)
 
   return (
     <>
@@ -78,8 +57,8 @@ const MyList = () => {
             <span><FolderOpenTwoTone color /> html</span>
             <span><FireTwoTone color />10000</span>
           </div>
-          <div>
-            <ReactMarkdown children={markdown} />
+          <div dangerouslySetInnerHTML={{__html:html}}>
+
           </div>
           <Footer></Footer>
         </Col>
@@ -91,7 +70,7 @@ const MyList = () => {
               <div className={sty.navTitle}>文章目录</div>
               <Divider></Divider>
               <MarkdownNavbar
-                source={markdown}
+                source={html}
                 ordered={false}
                 className={sty.navContent}
               />
@@ -103,7 +82,20 @@ const MyList = () => {
     </>
   )
 }
+Detailed.getInitialProps = async (context) => {
+  console.log(context.query.id)
+  let id = context.query.id
+  const promise = new Promise((resolve) => {
+    axios('http://127.0.0.1:7001/default/getArticleById/' + id).then(
+      (res) => {
+        console.log(res)
+        resolve(res.data.data[0])
+      }
+    )
+  })
 
+  return await promise
+}
 
-export default MyList
+export default Detailed
 
